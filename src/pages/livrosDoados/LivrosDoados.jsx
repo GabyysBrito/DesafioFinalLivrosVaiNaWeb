b549289f59effec305f42eed5b482b8c49a090d1
GabyysBrito <gaby.oliveira.brito1289@gmail.com>
2024-11-29 10:44:37 -0300

    O arquivo LivrosDoados completo e o responsivo do Header completo

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,5 +1,22 @@
+import livrosDoados from '../../assets/livrosDoados.png'
+import s from './livrosDoados.module.scss'
+
 export default function LivrosDoados(){
     return(
-        <h2>Eu sou camponentes livros doados</h2>
+        <section className={s.livrosDoadosSection}>
+            <h2>Livros Doados</h2>
+
+            <section className={s.containerCards}>
+                <section>
+                    <img src={livrosDoados} alt="Imagem do livro o Protagonista" />
+                    
+                    <div>
+                        <h3>O protagonista</h3>
+                        <p>Susane Andrade</p>
+                        <p>Ficção</p>
+                    </div>
+                </section>
+            </section>
+        </section>
     )
 }
