f3f3bae397b2879979757c34801741a3a8aef0ad
GabyysBrito <gaby.oliveira.brito1289@gmail.com>
2024-12-04 09:46:05 -0300

    Complementando alt dos icones do Footer

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -12,11 +12,11 @@ export default function Footer(){
             <section className={s.contatosFooter}>
                 <p>4002-8922</p>
                 <nav>
-                    <a href=""><img src={iconeFacebook} alt="" /></a>
-                    <a href=""><img src={iconeTwitter} alt="" /></a>
-                    <a href=""><img src={iconeYoutube} alt="" /></a>
-                    <a href=""><img src={iconeLinkedln} alt="" /></a>
-                    <a href=""><img src={iconeInstagram} alt="" /></a>
+                    <a href=""><img src={iconeFacebook} alt="Logo de icone do Facebook" /></a>
+                    <a href=""><img src={iconeTwitter} alt="Logo de icone do Twitter" /></a>
+                    <a href=""><img src={iconeYoutube} alt="Logo de icone do Youtube" /></a>
+                    <a href=""><img src={iconeLinkedln} alt="Logo de icone do Linkedln" /></a>
+                    <a href=""><img src={iconeInstagram} alt="Logo de icone do Instagram" /></a>
                 </nav>    
             </section>
             
